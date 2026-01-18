ed0b14224f24e41036975d9ea9d8fcb2163af414
Marcus von Gryczewski <marcus.gryczewski@gmail.com>
2026-01-18 19:40:24 +0100

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/FahrlaessigkeitPMLP/",
   build: {
-    sourcemap: true
+    sourcemap: true,
   },
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
 });
